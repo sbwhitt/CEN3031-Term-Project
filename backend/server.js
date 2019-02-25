e0d07e6cd28fc3742cd2debced488c3e8e19916f
Samuel Whittenberger <sbw98978@gmail.com>
2019-02-25 16:19:48 -0500

    remove unnecessary libraries

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const bodyParser = require("body-parser");
-const logger = require("morgan");
 const schema = require("./schema.js");
 
 const port = 3001;
@@ -23,12 +21,6 @@ db.once("open", () => console.log("connected to the database"));
 
 //checks if connection with the database is successful
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
-
-//(optional) only made for logging and
-//bodyParser, parses the request body to be a readable json format
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(logger("dev"));
 
 /*database post, put, delete, etc. functions will be implemented here
 
