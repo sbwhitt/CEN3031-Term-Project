de78a9b0b6916f02e045e187c24db37bd96aa7f6
Samuel Whittenberger <sbw98978@gmail.com>
2019-03-14 19:43:22 -0400

    make pages folder with example welcome page content

---
+++ src/pages/MemberPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class MemberPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        Members
+      </div>
+    );
+  }
+}
+
+export default MemberPage;
---
+++ src/pages/ProfilePage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class ProfilePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        Profile
+      </div>
+    );
+  }
+}
+
+export default ProfilePage;
