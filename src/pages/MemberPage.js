a48ead46cc2a9bcee8c0d774d48deb727792d567
Samuel Whittenberger <sbw98978@gmail.com>
2019-03-25 17:10:34 -0400

    displaying member data from db

--- src/pages/MemberPage.js
+++ src/pages/MemberPage.js
@@ -2,20 +2,6 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import ProfilePic from "./pfp_placeholder.jpg";
 
-var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-var testArr = [
-  {firstName: "George", lastName: "Washington", quote: lorem},
-  {firstName: "Abraham", lastName: "Lincoln", quote: lorem},
-  {firstName: "John", lastName: "Adams", quote: lorem},
-  {firstName: "Dwight", lastName: "Eisenhower", quote: lorem},
-  {firstName: "Herbert", lastName: "Hoover", quote: lorem},
-  {firstName: "Randy", lastName: "Jackson", quote: lorem},
-  {firstName: "Barack", lastName: "Obama", quote: lorem},
-  {firstName: "Jeff", lastName: "Goldblum", quote: lorem},
-  {firstName: "George", lastName: "Bush", quote: lorem},
-  {firstName: "Bill", lastName: "Clinton", quote: lorem}
-];
-
 const MemberItem = (props) => {
   var url = "/profile/" + props.item.firstName + props.item.lastName;
   return (
@@ -25,7 +11,7 @@ const MemberItem = (props) => {
     }} style={{color: "black"}}>
     <div className="member-container">
         <div className="member-card">
-          <img className="member-img" src={ProfilePic} alt="member profile"/>
+          <img className="member-img" src={props.item.image} alt="member profile"/>
           <div className="member-text">
             <h2>{props.item.firstName} {props.item.lastName}</h2>
             <p>{props.item.quote}</p>
@@ -56,10 +42,6 @@ class MemberPage extends Component {
 
   componentDidMount() {
     this.getMembers();
-    /*if (!this.state.interval) {
-      let interval = setInterval(this.getMembers, 1000);
-      this.setState({ interval: interval });
-    }*/
   }
 
   getMembers = () => {
@@ -96,7 +78,7 @@ class MemberPage extends Component {
           <h1 className="page-text">Members</h1>
           <hr className="page-divider"/>
           <MemberSearch onChange={(e) => this._onSearchChange(e)}/>
-          {this._renderItems(testArr, this.state.currentQuery)}
+          {this._renderItems(this.state.members, this.state.currentQuery)}
         </div>
       </div>
     );
