f5aeb92991ebf632083e64aeba2e9db5cd5c2c36
Edwin_Carvajal <EdwinCarvajal@Edwins-MacBook-Pro-2.local>
2019-03-31 19:12:59 -0400

    Get requestst for events and members list completed. Functionality for rest of put/delete/update not functional

--- backend/Controller/members.server.controller.js
+++ backend/Controller/members.server.controller.js
@@ -1,87 +1,105 @@
-var mongoose = require('mongoose'), 
-    Member = require('../Model/ProfileSchema.js');
+var mongoose = require('mongoose'),
+  Member = require('../Model/ProfileSchema.js');
 
 /* Create a member */
-exports.create = function(req, res) {
+exports.create = function (req, res) {
 
-    /* Instantiate a Member */
-    var member = new Member(req.body);
-  
-  
-    /* Then save the member */
-    member.save(function(err) {
-      if(err) {
-        console.log(err);
-        res.status(400).send(err);
-      } else {
-        res.json(member);
-      }
-    });
-  };
+  /* Instantiate a Member */
+  var member = new Member(req.body);
 
-  /* Show the current member */
-exports.read = function(req, res) {
-    /* send back the listing as json from the request */
-    res.json(req.member);
+
+  /* Then save the member */
+  member.save(function (err) {
+    if (err) {
+      console.log(err);
+      res.status(400).send(err);
+    } else {
+      res.json(member);
+    }
+  });
 };
 
-  /* Update a member */
-exports.update = function(req, res) {
-    var member = req.member;
-  
-      member.firstName           = req.body.firstName;
-      member.lastName            = req.body.lastName;
-      member.isActive            = req.body.isActive;
-      member.phoneNumber         = req.body.phoneNumber;
-      member.email               = req.body.email;
-      member.programs            = req.body.programs;
-      member.country             = req.body.country;
-      member.graduationSemester  = req.body.graduationSemester;
-      member.yearsLeft           = req.body.yearsLeft;
-      member.inducted            = req.body.inducted;
-      member.birthday            = req.body.birthday;
-      member.majors              = req.body.majors;
-      member.minors              = req.body.minors;
-      member.questions           = req.body.questions;
-      member.image               = req.body.image;
-      member.officeHours         = req.body.officeHours;
-      member.isAdmin             = req.body.isAdmin;
-      member.isExecutive         = req.body.isExecutive;
-      member.eventAttendedbyId   = req.body.eventAttendedbyId;
-      member.points              = req.body.points;
-  
-      member.save(function(err) {
-        if(err) {
-          console.log(err);
-          res.status(400).send(err);
-        } else {
-          res.json(member);
-        }
-      });
-  
-  };
+/* Show the current member */
+exports.read = function (req, res) {
+  /* send back the listing as json from the request */
+  res.json(req.member);
+};
 
-  // Delete a member
-  exports.delete = function(req, res) {
-    var member = req.member;
+/* Update a member */
+exports.update = function (req, res, firstLast) {
+  var member = req.member;
 
-    member.remove(function(err){
-      if(err){
-        console.log(err);
-        res.status(404).send(err); 
-        throw err;
-      }
-  
-      console.log("Member successfully removed");
-      res.end();
-    })
-  };
+  Member.findOneAndUpdate({
+    firstLast: firstLast
+  }, {
+    //firstName = req.body.firstName
+  }).exec(function (err, member) {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      // req.member = member;
+      next();
+    }
+  });
 
-  /* Retrieve all the members, sorted alphabetically by firstName */
-exports.listSorted = function(req, res) {
- 
-  Member.find().sort({lastName: 'asc'}).exec(function(err, data){
-    if(err){
+
+  // var member = req.member;
+
+  //   member.firstName           = req.body.firstName;
+  //   member.lastName            = req.body.lastName;
+  //   member.isActive            = req.body.isActive;
+  //   member.phoneNumber         = req.body.phoneNumber;
+  //   member.email               = req.body.email;
+  //   member.programs            = req.body.programs;
+  //   member.country             = req.body.country;
+  //   member.graduationSemester  = req.body.graduationSemester;
+  //   member.yearsLeft           = req.body.yearsLeft;
+  //   member.inducted            = req.body.inducted;
+  //   member.birthday            = req.body.birthday;
+  //   member.majors              = req.body.majors;
+  //   member.minors              = req.body.minors;
+  //   member.questions           = req.body.questions;
+  //   member.image               = req.body.image;
+  //   member.officeHours         = req.body.officeHours;
+  //   member.isAdmin             = req.body.isAdmin;
+  //   member.isExecutive         = req.body.isExecutive;
+  //   member.eventAttendedbyId   = req.body.eventAttendedbyId;
+  //   member.points              = req.body.points;
+
+  member.save(function (err) {
+    if (err) {
+      console.log(err);
+      res.status(400).send(err);
+    } else {
+      res.json(member);
+    }
+  });
+
+};
+
+// Delete a member
+exports.delete = function (req, res) {
+  var member = req.member;
+
+  member.remove(function (err) {
+    if (err) {
+      console.log(err);
+      res.status(404).send(err);
+      throw err;
+    }
+
+    console.log("Member successfully removed");
+    res.end();
+  })
+};
+
+/* Retrieve all the members, sorted alphabetically by firstName */
+exports.listSorted = function (req, res) {
+
+  Member.find().sort({
+    lastName: 'asc'
+  }).exec(function (err, data) {
+    if (err) {
       res.status(404).send(err);
       throw err;
     }
@@ -91,21 +109,32 @@ exports.listSorted = function(req, res) {
 };
 
 //Retrieve all members without sort
-exports.list = function(req, res){
+exports.list = function (req, res) {
   Member.find((err, data) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true, data: data });
+    if (err) return res.json({
+      success: false,
+      error: err
+    });
+    return res.json({
+      success: true,
+      data: data
+    });
   });
 }
 
 //Change the permissions of a member to executive
-exports.promoteExecutive = function(req, res){
+exports.promoteExecutive = function (req, res) {
   Member.findAndModify({
-    query: {firstName: req.body.firstName, lastName: req.body.lastName}, 
-    update: {isExecutive: true}, 
+    query: {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName
+    },
+    update: {
+      isExecutive: true
+    },
     upsert: true
-  }).exec(function(err, data){
-    if(err){
+  }).exec(function (err, data) {
+    if (err) {
       res.stats(404).send(err);
       throw err;
     }
@@ -117,13 +146,18 @@ exports.promoteExecutive = function(req, res){
 };
 
 //Change the permissions of an executive to member
-exports.demoteExecutive = function(req, res){
+exports.demoteExecutive = function (req, res) {
   Member.findAndModify({
-    query: {firstName: req.body.firstName, lastName: req.body.lastName}, 
-    update: {isExecutive: false}, 
+    query: {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName
+    },
+    update: {
+      isExecutive: false
+    },
     upsert: true
-  }).exec(function(err, data){
-    if(err){
+  }).exec(function (err, data) {
+    if (err) {
       res.stats(404).send(err);
       throw err;
     }
@@ -134,13 +168,12 @@ exports.demoteExecutive = function(req, res){
   })
 };
 
-exports.memberByFirstLast = function(req, res, next, first, last) {
-  console.log(first + last);
+exports.memberByFirstLast = function (req, res, next, firstLast) {
+  console.log(firstLast);
   Member.findOne({
-    firstName: first,
-    lastName: last
-  }).exec(function(err, member) {
-    if(err) {
+    firstLast: firstLast
+  }).exec(function (err, member) {
+    if (err) {
       res.status(400).send(err);
     } else {
       req.member = member;
