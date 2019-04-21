bdf395ba842cabb9e3dfe6ac7da1ab32ceaef077
Edwin_Carvajal <EdwinCarvajal@Edwins-MacBook-Pro-2.local>
2019-04-21 16:56:42 -0400

    Merge conflict resolved from updated Development branch and chasebranch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React, {
+  Component
+} from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
 import WelcomePage from './pages/WelcomePage.js';
 import MemberPage from './pages/MemberPage.js';
 import EventPage from './pages/EventPage.js';
@@ -24,7 +30,9 @@ also broken when refreshing page
 if (localStorage.jwt) {
   const token = localStorage.getItem('jwt');
   const now = Date.now() / 1000;
-  axios.post("/api/auth/decode", {token: token})
+  axios.post("/api/auth/decode", {
+      token: token
+    })
     .then((res) => {
       if (res.data.data.exp < now) {
         localStorage.removeItem('jwt');
@@ -39,22 +47,26 @@ class HeaderBar extends Component {
       selected: "Home",
       currentUser: {},
       profile: this.props.profile,
-	  }
+    }
   }
 
   componentDidMount() {
     this._checkLogInStatus();
   }
-  
+
   //checks if a jwt is present
   //if jwt exists, loads token and sends it to backend for decodeing
   //on successful decode, takes payload and puts it into this.state.currentUser
   _checkLogInStatus = () => {
     if (localStorage.jwt) {
       const token = localStorage.getItem("jwt");
-      axios.post("/api/auth/decode", {token: token}).then((res) => {
+      axios.post("/api/auth/decode", {
+        token: token
+      }).then((res) => {
         if (res.data) {
-          this.setState({currentUser: res.data.data});
+          this.setState({
+            currentUser: res.data.data
+          });
         }
       });
     }
@@ -63,26 +75,80 @@ class HeaderBar extends Component {
   //changes which hlink is underlined
   //kinda broken, need to switch based on url instead
   selectHLink = name => {
-    this.setState({selected: name});
+    this.setState({
+      selected: name
+    });
   }
 
   render() {
     const headerRight = this.state.currentUser.email ?
-      <ProfileCircle image={this.props.profile.image} link={'/profile/' + this.props.profile.firstLast}/> :
-      <button className="login-button" onClick={this.props.loginClick}>Log In</button>;
-    
-    return (
-      <div className="header-bar">
-        <div style={{display: "flex", flexDirection: "row"}}>
-          <Logo onClick={this.selectHLink}/>
-          <HLink onClick={this.selectHLink} selected={this.state.selected} name="Home" link="/"/>
-          <HLink onClick={this.selectHLink} selected={this.state.selected} name="Members" link="/members"/>
-          <HLink onClick={this.selectHLink} selected={this.state.selected} name="Events" link="/events"/>
-          <HLink onClick={this.selectHLink} selected={this.state.selected} name="About" link="/about"/>
-          {/*<HLink onClick={this.selectHLink} selected={this.state.selected} name="Management" link="/management"/>*/}
-        </div>
-        {headerRight}
-      </div>
+      <
+      ProfileCircle image = {
+        this.props.profile.image
+      }
+    link = {
+      '/profile/' + this.props.profile.firstLast
+    }
+    /> : <
+    button className = "login-button"
+    onClick = {
+      this.props.loginClick
+    } > Log In < /button>;
+
+    return ( <
+      div className = "header-bar" >
+      <
+      div style = {
+        {
+          display: "flex",
+          flexDirection: "row"
+        }
+      } >
+      <
+      Logo onClick = {
+        this.selectHLink
+      }
+      /> <
+      HLink onClick = {
+        this.selectHLink
+      }
+      selected = {
+        this.state.selected
+      }
+      name = "Home"
+      link = "/" / >
+      <
+      HLink onClick = {
+        this.selectHLink
+      }
+      selected = {
+        this.state.selected
+      }
+      name = "Members"
+      link = "/members" / >
+      <
+      HLink onClick = {
+        this.selectHLink
+      }
+      selected = {
+        this.state.selected
+      }
+      name = "Events"
+      link = "/events" / >
+      <
+      HLink onClick = {
+        this.selectHLink
+      }
+      selected = {
+        this.state.selected
+      }
+      name = "About"
+      link = "/about" / > {
+        /*<HLink onClick={this.selectHLink} selected={this.state.selected} name="Management" link="/management"/>*/ } <
+      /div> {
+        headerRight
+      } <
+      /div>
     );
   }
 }
@@ -92,7 +158,7 @@ class ProfileCircle extends Component {
     super(props);
     this.state = {
       isOpen: false,
-	  }
+    }
   }
 
   _logOut = () => {
@@ -101,46 +167,113 @@ class ProfileCircle extends Component {
   }
 
   _toggleMenu = () => {
-    this.setState({isOpen: !this.state.isOpen});
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
   }
 
-  render () {
-    return (
-      <div>
-        <div><img onClick={this._toggleMenu} className="circle-img" src={this.props.image}/></div>
-        <ProfileMenu onClick={this._toggleMenu} onLogout={this._logOut} isOpen={this.state.isOpen} link={this.props.link}/>
-      </div>
+  render() {
+    return ( <
+      div >
+      <
+      div > < img onClick = {
+        this._toggleMenu
+      }
+      className = "circle-img"
+      src = {
+        this.props.image
+      }
+      /></div >
+      <
+      ProfileMenu onClick = {
+        this._toggleMenu
+      }
+      onLogout = {
+        this._logOut
+      }
+      isOpen = {
+        this.state.isOpen
+      }
+      link = {
+        this.props.link
+      }
+      /> <
+      /div>
     );
   }
 }
 
 const ProfileMenu = (props) => {
-  return props.isOpen ? (
-    <div className="profile-menu">
-      <Link onClick={() => {props.onClick(); window.location.assign(props.link)}} className="profile-menu-opt" to={props.link}>
-        <span>View Profile</span>
-      </Link>
-      <span onClick={() => {props.onClick(); props.onLogout()}} className="profile-menu-opt">Log Out</span>
-    </div>
+  return props.isOpen ? ( <
+    div className = "profile-menu" >
+    <
+    Link onClick = {
+      () => {
+        props.onClick();
+        window.location.assign(props.link)
+      }
+    }
+    className = "profile-menu-opt"
+    to = {
+      props.link
+    } >
+    <
+    span > View Profile < /span> <
+    /Link> <
+    span onClick = {
+      () => {
+        props.onClick();
+        props.onLogout()
+      }
+    }
+    className = "profile-menu-opt" > Log Out < /span> <
+    /div>
   ) : null;
 }
 
 //headerbar link (home, members, etc.)
 const HLink = (props) => {
-  return (
-    <Link onClick={() => props.onClick(props.name)} className="hlink" 
-      style={props.selected === props.name ? {textDecoration: "underline"} : {}} to={props.link}>
-      <p style={{margin: "14px 0 0 0"}}>{props.name}</p>
-    </Link>
+  return ( <
+    Link onClick = {
+      () => props.onClick(props.name)
+    }
+    className = "hlink"
+    style = {
+      props.selected === props.name ? {
+        textDecoration: "underline"
+      } : {}
+    }
+    to = {
+      props.link
+    } >
+    <
+    p style = {
+      {
+        margin: "14px 0 0 0"
+      }
+    } > {
+      props.name
+    } < /p> <
+    /Link>
   );
 }
 
 //uf logo on headerbar
 const Logo = (props) => {
-  return (
-    <Link onClick={() => props.onClick("Home")} className="logo-container" to="/">
-      <img className="logo-img" src={UFLogo} alt="UF Logo"/>
-    </Link>
+  return ( <
+    Link onClick = {
+      () => props.onClick("Home")
+    }
+    className = "logo-container"
+    to = "/" >
+    <
+    img className = "logo-img"
+    src = {
+      UFLogo
+    }
+    alt = "UF Logo" / >
+    <
+    /Link>
   );
 }
 
@@ -152,19 +285,23 @@ class App extends Component {
       loginVisible: false,
       currentUser: {},
       profile: {},
-	  }
+    }
   }
 
   componentDidMount() {
     this._checkLogInStatus();
   }
-  
+
   _checkLogInStatus = () => {
     if (localStorage.jwt) {
       const token = localStorage.getItem("jwt");
-      axios.post("/api/auth/decode", {token: token}).then((res) => {
+      axios.post("/api/auth/decode", {
+        token: token
+      }).then((res) => {
         if (res.data) {
-          this.setState({currentUser: res.data.data});
+          this.setState({
+            currentUser: res.data.data
+          });
           this._getProfile(res.data.data.email);
         }
       });
@@ -177,40 +314,107 @@ class App extends Component {
         email: email
       }
     }).then((res) => {
-      this.setState({profile: res.data});
+      this.setState({
+        profile: res.data
+      });
     });
   }
 
   render() {
-    return (
-      <Router>
-        <div className="App">
-          <LoginWindow isVisible={this.state.loginVisible} onClose={(e) => this.setState({loginVisible: !this.state.loginVisible})}/>
-          <HeaderBar profile={this.state.profile} loginClick={(e) => this.setState({loginVisible: !this.state.loginVisible})}/>
-          <div style={{marginTop: "1.5em"}}>
-<<<<<<< HEAD
-            <Route exact path="/" render={(props) => <WelcomePage {...props} currentUser={this.state.currentUser}/>}/>
-            <Route path="/members" render={(props) => <MemberPage {...props} currentUser={this.state.currentUser}/>}/>
-            <Route path="/events" render={(props) => <EventPage {...props} currentUser={this.state.currentUser}/>}/>
-            <Route path="/profile" render={(props) => <ProfilePage {...props} currentUser={this.state.currentUser}/>}/>
-            <Route path="/event" render={(props) => <EventInfoPage {...props} currentUser={this.state.currentUser}/>}/>
-            <Route path="/about" render={(props) => <AboutPage {...props} currentUser={this.state.currentUser}/>}/>
-            {/*<Route path="/management" component={ManagementPage}/>*/}
-=======
-            <Route exact path="/" component={WelcomePage}/>
-            <Route path="/members" component={MemberPage}/>
-            <Route path="/events" component={EventPage}/>
-            <Route path="/profile" component={ProfilePage}/>
-            <Route path="/event" component={EventInfoPage}/>
-            <Route path="/about" component={AboutPage}/>
-            <Route path="/management" component={ManagementPage}/>
-            <Route path="/alumni" component={AlumniPage}/>
->>>>>>> a087db17276bde5994e22ae54c5dd6799ab83f2d
-          </div>
-        </div>
-      </Router>
-    );
-  }
-}
+      return ( <
+          Router >
+          <
+          div className = "App" >
+          <
+          LoginWindow isVisible = {
+            this.state.loginVisible
+          }
+          onClose = {
+            (e) => this.setState({
+              loginVisible: !this.state.loginVisible
+            })
+          }
+          /> <
+          HeaderBar profile = {
+            this.state.profile
+          }
+          loginClick = {
+            (e) => this.setState({
+              loginVisible: !this.state.loginVisible
+            })
+          }
+          /> <
+          div style = {
+            {
+              marginTop: "1.5em"
+            }
+          } >
+          <
+          Route exact path = "/"
+          render = {
+            (props) => < WelcomePage {
+              ...props
+            }
+            currentUser = {
+              this.state.currentUser
+            }
+            />}/ >
+            <
+            Route path = "/members"
+            render = {
+              (props) => < MemberPage {
+                ...props
+              }
+              currentUser = {
+                this.state.currentUser
+              }
+              />}/ >
+              <
+              Route path = "/events"
+              render = {
+                (props) => < EventPage {
+                  ...props
+                }
+                currentUser = {
+                  this.state.currentUser
+                }
+                />}/ >
+                <
+                Route path = "/profile"
+                render = {
+                  (props) => < ProfilePage {
+                    ...props
+                  }
+                  currentUser = {
+                    this.state.currentUser
+                  }
+                  />}/ >
+                  <
+                  Route path = "/event"
+                  render = {
+                    (props) => < EventInfoPage {
+                      ...props
+                    }
+                    currentUser = {
+                      this.state.currentUser
+                    }
+                    />}/ >
+                    <
+                    Route path = "/about"
+                    render = {
+                      (props) => < AboutPage {
+                        ...props
+                      }
+                      currentUser = {
+                        this.state.currentUser
+                      }
+                      />}/ > {
+                        /*<Route path="/management" component={ManagementPage}/>*/ } <
+                      /div> <
+                      /div> <
+                      /Router>
+                    );
+                  }
+                }
 
-export default App;
+                export default App;
