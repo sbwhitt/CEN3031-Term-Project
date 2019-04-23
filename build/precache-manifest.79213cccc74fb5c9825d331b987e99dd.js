0abdcf41fb47a7d6ce4bc0f731dc5ca46cbcf976
Samuel Whittenberger <sbw98978@gmail.com>
2019-04-23 19:10:57 -0400

    build

---
+++ build/precache-manifest.79213cccc74fb5c9825d331b987e99dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0032e5c87e1d7bac37e6d650f7114bbd",
+    "url": "/static/media/instagram_icon.0032e5c8.png"
+  },
+  {
+    "revision": "bf38bd12d581c536b4f9da7e1b67dd44",
+    "url": "/static/media/earth_sapa.bf38bd12.gif"
+  },
+  {
+    "revision": "e22336a8f0c3066067b3c453e110212f",
+    "url": "/static/media/SAPA_welcome.e22336a8.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b2fbbe03a9c96f8906dd",
+    "url": "/static/js/main.b2fbbe03.chunk.js"
+  },
+  {
+    "revision": "5194626a33e5e49b0f07",
+    "url": "/static/js/2.5194626a.chunk.js"
+  },
+  {
+    "revision": "b2fbbe03a9c96f8906dd",
+    "url": "/static/css/main.b63b48c9.chunk.css"
+  },
+  {
+    "revision": "0b5475725d17afc4e4e1a11f6ff31f0d",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.168b93c311a32fa12bed83a9217a3e5e.js"
+  "/precache-manifest.79213cccc74fb5c9825d331b987e99dd.js"
 );
 
 workbox.clientsClaim();
